d0770b979bb1535eff7e6a6a3096c80360deccbb
MarioTury <mariotury@outlook.com>
2018-08-23 05:26:13 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app 		= require('express')();
-const server = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+const server = process.env.server || 3000;
+app.listen(serverT, () => {
+    console.log(`Our app is running on port ${ server }`);
 });
 //server 		= app.listen(3300),
 var io 			= require('socket.io')(server),
