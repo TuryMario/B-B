2c783628581f28e99b02e1a9e2e5ba32f6150742
MarioTury <mariotury@outlook.com>
2018-08-23 05:34:06 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app 		= require('express')();
-const server = process.env.server || 3000;
+// const server = process.env.server || 3000;
+const server = 8080;
 app.listen(server, () => {
     console.log(`Our app is running on port ${ server }`);
 });
